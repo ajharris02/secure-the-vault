23ff781bc97c004a73287bfd3f76def5b1a2d1e4
Antonio Harris Jr <your_ajharris0201@gmail.com>
2026-06-10 09:54:02 -0400

    index.js changes

---
+++ index.js
@@ -0,0 +1,33 @@
+/*const code1 = 5;
+
+const code1 = 7 + 3;
+console.log(code1);
+
+const code2 = (100 - 20) / 2;
+console.log(code2);
+
+const code3 = (13 % 3) + 38;
+console.log(code3);
+
+const message = "The vault has been secured. The comnodbination is:";
+console.log(message);
+
+const code1 = "10";
+const code2 = "40";
+const code3 = "39";
+const codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA); 
+
+const code1 = "10";
+const code2 = "40";
+const code3 = "39";
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);*/
+
+const message = "The vault has been secured. The comnodbination is:";
+const code1 = "10";
+const code2 = "40";
+const code3 = "39";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
